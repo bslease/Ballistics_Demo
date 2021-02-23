c880e65c2c24f0b0c22a17f1ad1278547a5bd377
bslease <bslease@gmail.com>
2021-02-23 07:12:15 -0700

    Set up demo

---
+++ Ballistics Demo Project/Assets/Scripts/Frog.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Frog : MonoBehaviour
+{
+    public float myTimeScale = 1.0f;
+    public GameObject target;
+    public float launchForce = 10f;
+    Rigidbody rb;
+    Vector3 startPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+        Time.timeScale = myTimeScale; // allow for slowing time to see what's happening
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            FiringSolution fs = new FiringSolution();
+            Nullable<Vector3> aimVector = fs.Calculate(transform.position, target.transform.position, launchForce, Physics.gravity);
+            if (aimVector.HasValue)
+            {
+                rb.AddForce(aimVector.Value.normalized * launchForce, ForceMode.VelocityChange);
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            // reset
+            rb.isKinematic = true;
+            transform.position = startPos;
+            rb.isKinematic = false;
+        }
+    }
+}
